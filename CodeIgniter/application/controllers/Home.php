64aedd6df508a9a623cd6bcc775983de63f26544
ridarajani <ridarajani125@gmail.com>
2019-11-17 03:00:00 -0700

    css and js

---
+++ CodeIgniter/application/controllers/Home.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends MY_Controller {
+
+	public function index()
+	{
+        $data['view'] = 'Home';
+        $data['page_title'] = 'Home';
+        $this->load->view('Layout',$data);
+	}
+}
